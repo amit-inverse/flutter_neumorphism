844def97ec9defffe76f2f1e165e5062c7b9c8e0
Amit Kumar Das <amit.inverse@gmail.com>
2024-02-20 22:32:11 +0530

    002

--- lib/main.dart
+++ lib/main.dart
@@ -28,24 +28,28 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      // backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.grey[850],
       body: Center(
         child: Container(
           width: 200,
           height: 200,
           decoration: BoxDecoration(
-              color: Colors.grey[300],
+              // color: Colors.grey[300],
+              color: Colors.grey[850],
               borderRadius: const BorderRadius.all(
                 Radius.circular(50),
               ),
               boxShadow: const [
                 BoxShadow(
-                    color: Colors.grey,
+                    // color: Colors.grey,
+                    color: Colors.black87,
                     offset: Offset(4.0, 4.0),
                     blurRadius: 15.0,
                     spreadRadius: 1.0),
                 BoxShadow(
-                    color: Colors.white,
+                    // color: Colors.white,
+                    color: Colors.white12,
                     offset: Offset(-4.0, -4.0),
                     blurRadius: 15.0,
                     spreadRadius: 1.0),
@@ -53,6 +57,8 @@ class _MyHomePageState extends State<MyHomePage> {
           child: const Icon(
             Icons.android,
             size: 80,
+            // color: Colors.black,
+            color: Colors.white,
           ),
         ),
       ),
